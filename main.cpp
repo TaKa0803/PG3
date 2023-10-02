9d9c8f0625e0735a7f950e83e8bf784e417c26c1
Specha <warekosohasupecha1257@gmail.com>
2023-10-02 11:18:25 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
-#include<stdio.h>
+﻿#include<stdio.h>
+#include<ctype.h>
 
 template<typename T>
-T chckNum(T a, T b) {
+T checkNum(T a, T b) {	
+		if (a <= b) {
+			return a;
+		}
+		else {
+			return b;
+		}
+	
+}
 
+template<>
+char checkNum<char>(char a, char b) {
+	printf("数字以外は代入出来ません");
+	return'\n';
 }
 
 
-int main() {
 
+int main(void) {
+
+	printf("%d\n", checkNum<int>(10, 5));
+	printf("%4.1f\n", checkNum<float>(10.2f, 0.5f));
+	printf("%lf\n", checkNum<double>(10.2, 0.1));
+	printf("%c\n", checkNum('a', 'b'));
 
 	return 0;
 }
