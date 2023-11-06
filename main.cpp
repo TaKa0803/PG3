03dd66a3c2d0278963b7ec67e4afb3c8bc0a4798
TaKa0803 <k022g0068@g.neec.ac.jp>
2023-11-06 10:42:56 +0900

    a

--- main.cpp
+++ main.cpp
@@ -2,52 +2,56 @@
 #include<ctype.h>
 
 template<typename T>
-void checkNum(T a, T b) {	
-		if (a <= b) {
-			printf("%d\n", a);
-			return;
-		}
-		else {
-			printf("%d\n", b);
-		}
-	
-}
-
-template<>
-void checkNum<char>(char a, char b) {
-	
-	printf("数字以外は代入出来ません");
-	return;
-}
-
-template<>
-void checkNum<float>(float a, float b) {
+void checkNum(T a, T b) {
 	if (a <= b) {
-		printf("%f\n", a);
+
+		if (T == int) {
+			printf("%d", a);
+		}
+		if (T == float) {
+			printf("%f", a);
+		}if (T == double) {
+			printf("%lf", a);
+		}if (T == char) {
+			printf("数字以外は代入出来ません");
+		}
+
 		return;
 	}
 	else {
-		printf("%f\n", b);
-	}
-}
-template<>
-void checkNum<double>(double a, double b) {
-	if (a <= b) {
-		printf("%lf\n", a);
+		if (T == int) {
+			printf("%d", b);
+		}
+		if (T == float) {
+			printf("%f", b);
+		}if (T == double) {
+			printf("%lf", b);
+		}if (T == char) {
+			printf("数字以外は代入出来ません");
+		}
+
 		return;
 	}
-	else {
-		printf("%lf\n", b);
-	}
+
 }
+
+
+
 
 
 int main(void) {
+	/*
+	printf("%d\n", checkNum<int>(10, 5));
+	printf("%f\n", checkNum<float>(10.2f, 0.5f));
+	printf("%lf\n", checkNum<double>(10.2, 0.1));
+	printf("%c\n", checkNum<char>('a','b'));
+	*/
 
+	
 	checkNum<int>(10, 5);
 	checkNum<float>(10.2f, 0.5f);
 	checkNum<double>(10.2, 0.1);
 	checkNum('a', 'b');
-
+	
 	return 0;
 }
