05d6c55443c44744abd63617618ea34b469f434e
TaKa0803 <k022g0068@g.neec.ac.jp>
2023-11-07 23:50:48 +0900

    a

---
+++ IShape.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+class IShape {
+
+public:
+
+	virtual void size() = 0;
+
+	virtual void draw() = 0;
+
+private:
+
+	int size = 1;
+
+};
--- main.cpp
+++ main.cpp
@@ -1,35 +1,12 @@
 #include<stdio.h>
 
 
-#include"Human.h"
-#include"Friend.h"
-#include"Me.h"
+
 
 int main(void) {
 
 	
-	Human* humans[3];
-
-	for (int i = 0; i < 3; i++) {
-		if (i < 2) {
-			humans[i] = new Friend;
-		}
-		else {
-			humans[i] = new Me;
-		}
-	}
-	printf("\n");
 	
-	for (int i = 0; i < 3; i++) {
-		humans[i]->Action();
-	}
-	printf("\n");
-
-	for (int i = 0; i < 3; i++) {
-		delete humans[i];
-	}
-
-	printf("\n");
 
 	return 0;
 }
